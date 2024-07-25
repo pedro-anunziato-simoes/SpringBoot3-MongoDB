ced6c802e6bc2d851b11e529a9508ebcb131ff00
User <pedro.anunziato.simoes@gmail.com>
2024-07-25 17:05:53 -0300

    Instanciação da base de dados

---
+++ src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.MongoDb_Java_Project.MongoDb_Java_Project.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.MongoDb_Java_Project.MongoDb_Java_Project.domain.User;
+import com.MongoDb_Java_Project.MongoDb_Java_Project.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userReposiroty;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userReposiroty.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
