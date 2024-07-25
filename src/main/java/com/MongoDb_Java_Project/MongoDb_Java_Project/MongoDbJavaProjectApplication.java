60d0024a838e26bdbd81520ff02427b594c9d33c
User <pedro.anunziato.simoes@gmail.com>
2024-07-25 16:08:36 -0300

    Primeiro commit, criação do repositório

---
+++ src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/MongoDbJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.MongoDb_Java_Project.MongoDb_Java_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbJavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MongoDb_Java_Project/MongoDb_Java_Project/MongoDbJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MongoDb_Java_Project.MongoDb_Java_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
