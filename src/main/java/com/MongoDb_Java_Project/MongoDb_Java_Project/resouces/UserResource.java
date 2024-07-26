6b7bca7a61e60269c5d3e5dd54694f96b95a7372
User <pedro.anunziato.simoes@gmail.com>
2024-07-26 17:01:43 -0300

    Inserção de usuario com a requisição POST

--- src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/resouces/UserResource.java
+++ src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/resouces/UserResource.java
@@ -1,5 +1,6 @@
 package com.MongoDb_Java_Project.MongoDb_Java_Project.resouces;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,12 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.MongoDb_Java_Project.MongoDb_Java_Project.domain.User;
 import com.MongoDb_Java_Project.MongoDb_Java_Project.dto.UserDTO;
 import com.MongoDb_Java_Project.MongoDb_Java_Project.services.UserService;
+
+import jakarta.servlet.Servlet;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -24,15 +30,22 @@ public class UserResource {
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		List<UserDTO> listDto = list.stream().map(x-> new UserDTO(x)).collect(Collectors.toList());
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	@GetMapping(value="/{id}")
+
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-	
-	
+
+	@PostMapping
+	public ResponseEntity<Void> isert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/services/UserService.java
+++ src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.MongoDb_Java_Project.MongoDb_Java_Project.domain.User;
+import com.MongoDb_Java_Project.MongoDb_Java_Project.dto.UserDTO;
 import com.MongoDb_Java_Project.MongoDb_Java_Project.repository.UserRepository;
 import com.MongoDb_Java_Project.MongoDb_Java_Project.services.exception.ObjectNotFoundException;
 
@@ -21,5 +22,13 @@ public class UserService {
 
 	public User findById(String id) {
 		return repo.findById(id).orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
 }
