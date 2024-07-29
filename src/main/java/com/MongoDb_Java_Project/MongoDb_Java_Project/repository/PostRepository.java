9c6b79418b3c50bd035937c06cab00cec39a8088
User <pedro.anunziato.simoes@gmail.com>
2024-07-29 16:27:04 -0300

    Consulta simples com o @Querry

--- src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/repository/PostRepository.java
+++ src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.MongoDb_Java_Project.MongoDb_Java_Project.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.MongoDb_Java_Project.MongoDb_Java_Project.domain.Post;
@@ -10,6 +11,9 @@ import com.MongoDb_Java_Project.MongoDb_Java_Project.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title':{$regex: ?0,$options: 'i'}}")
+	List<Post> findByTitle(String text);
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
-	
+
 }
