440cf84e0b19c92093513db9d2598a3574439fae
User <pedro.anunziato.simoes@gmail.com>
2024-07-29 11:33:04 -0300

    Projeção dos dados DTO

--- src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/config/Instantiation.java
+++ src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/config/Instantiation.java
@@ -1,9 +1,7 @@
 package com.MongoDb_Java_Project.MongoDb_Java_Project.config;
 
 import java.sql.Date;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.MongoDb_Java_Project.MongoDb_Java_Project.domain.Post;
 import com.MongoDb_Java_Project.MongoDb_Java_Project.domain.User;
+import com.MongoDb_Java_Project.MongoDb_Java_Project.dto.AuthorDTO;
 import com.MongoDb_Java_Project.MongoDb_Java_Project.repository.PostRepository;
 import com.MongoDb_Java_Project.MongoDb_Java_Project.repository.UserRepository;
 
@@ -26,8 +25,6 @@ public class Instantiation implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userReposiroty.deleteAll();
 		postRepository.deleteAll();
@@ -36,12 +33,13 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null,"Bob Grey", "bob@gmail.com");
 		
-		
-		Post post1 = new Post(null, new Date(29/07/2024),"Partiu Viagem!","Vou viajar para São Paulo",maria);
-		Post post2 = new Post(null, new Date(29/07/2024),"Bom dia!","Acordei feliz hoje!",maria);
-		
-		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, new Date(29/07/2024),"Partiu Viagem!","Vou viajar para São Paulo",new AuthorDTO(maria));
+		Post post2 = new Post(null, new Date(29/07/2024),"Bom dia!","Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
 
---
+++ src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/dto/AuthorDTO.java
@@ -0,0 +1,38 @@
+package com.MongoDb_Java_Project.MongoDb_Java_Project.dto;
+
+import java.io.Serializable;
+
+import com.MongoDb_Java_Project.MongoDb_Java_Project.domain.User;
+
+public class AuthorDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String name;
+
+	public AuthorDTO() {
+
+	}
+
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
