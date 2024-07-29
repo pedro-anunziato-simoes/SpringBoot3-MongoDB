161fba69aa9770f70d3e33c2392c9a8ebcf1aab7
User <pedro.anunziato.simoes@gmail.com>
2024-07-29 15:37:34 -0300

    Adicionar o metodo de obter um post pelo ID

---
+++ src/main/java/com/MongoDb_Java_Project/MongoDb_Java_Project/services/PostService.java
@@ -0,0 +1,20 @@
+package com.MongoDb_Java_Project.MongoDb_Java_Project.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.MongoDb_Java_Project.MongoDb_Java_Project.domain.Post;
+import com.MongoDb_Java_Project.MongoDb_Java_Project.repository.PostRepository;
+import com.MongoDb_Java_Project.MongoDb_Java_Project.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+
+}
